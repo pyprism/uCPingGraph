d22f1d1b50b6467ec49816ddc37e7697ad790118
Ashutosh Das <git.pyprism@gmail.com>
2023-08-05 23:32:44 +0600

    Added server command

---
+++ server/cmd/server.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"github.com/pyprism/uCPingGraph/routers"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+}
+
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Start the server",
+	Long:  `Start the server to serve the web app.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		routers.Init()
+	},
+}
