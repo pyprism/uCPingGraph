e22fc9a55abde35eef2b71eb7b7b3de33248ebd6
Ashutosh Das <git.pyprism@gmail.com>
2023-08-05 23:26:55 +0600

    added router without route lol

---
+++ server/routers/routers.go
@@ -0,0 +1,43 @@
+package routers
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/gzip"
+	limits "github.com/gin-contrib/size"
+	ginzap "github.com/gin-contrib/zap"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"os"
+	"time"
+)
+
+func NewRouter() *gin.Engine {
+	var logger *zap.Logger
+
+	router := gin.New()
+	router.Static("/static", "./static")
+	router.LoadHTMLGlob("templates/*.html")
+
+	if os.Getenv("DEBUG") != "True" {
+		gin.SetMode(gin.ReleaseMode)
+		logger, _ = zap.NewProduction()
+		router.Use(ginzap.Ginzap(logger, time.RFC3339, true))
+		router.Use(ginzap.RecoveryWithZap(logger, true))
+	}
+	
+	router.SetTrustedProxies([]string{"127.0.0.1"})
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+	router.Use(cors.Default())
+	router.Use(gzip.Gzip(gzip.BestCompression))
+	router.Use(limits.RequestSizeLimiter(10000)) // 10KB
+
+	// for debug
+	router.GET("/status/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"hi": "hiren",
+		})
+	})
+
+	return router
+}
