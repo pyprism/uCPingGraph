48bf05dc027f0f29888c0a28a8514557e44c8209
Ashutosh Das <git.pyprism@gmail.com>
2023-08-05 23:26:38 +0600

    added router config

---
+++ server/routers/config.go
@@ -0,0 +1,16 @@
+package routers
+
+import (
+	"fmt"
+	"github.com/pyprism/uCPingGraph/utils"
+)
+
+func Init() {
+	r := NewRouter()
+	serverPort := utils.GetEnv("SERVER_PORT", "8080")
+	fmt.Println("Server running on http://127.0.0.1" + serverPort)
+	err := r.Run(serverPort)
+	if err != nil {
+		panic(err)
+	}
+}
