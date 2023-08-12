e359f5bd0dc955864637f945306ce70b1498506a
Ashutosh Das <git.pyprism@gmail.com>
2023-08-12 22:22:40 +0600

    added model method

--- server/models/network.go
+++ server/models/network.go
@@ -1,8 +1,21 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"log"
+)
 
 type Network struct {
 	gorm.Model
-	Name string `gorm:"index:unique;not null;size:500"`
+	Name string `gorm:"unique;not null;size:500"`
+}
+
+func (n *Network) CreateNetwork(name string) (error, uint) {
+	n.Name = name
+	err := DB.Create(n)
+	if err != nil {
+		log.Println("Failed to create network! Error: ", err.Error)
+		return err.Error, 0
+	}
+	return nil, n.ID
 }
