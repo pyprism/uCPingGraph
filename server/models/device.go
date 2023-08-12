09947a8b82c445edf23c0b03d82d1787cc1bde08
Ashutosh Das <git.pyprism@gmail.com>
2023-08-12 23:39:02 +0600

    added model method

--- server/models/device.go
+++ server/models/device.go
@@ -1,11 +1,26 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/pyprism/uCPingGraph/utils"
+	"gorm.io/gorm"
+)
 
 type Device struct {
 	gorm.Model
 	NetworkID int
 	Network   Network
 	Name      string `gorm:"not null;size:500"`
-	Token     string `gorm:"not null;size:15"`
+	Token     string `gorm:"index;not null;size:15"`
+}
+
+func (d *Device) CreateDevice(networkID int, name string) (error, uint, string) {
+	token := utils.GenToken(15)
+	d.NetworkID = networkID
+	d.Name = name
+	d.Token = token
+	err := DB.Create(d)
+	if err != nil {
+		return err.Error, 0, ""
+	}
+	return nil, d.ID, d.Token
 }
