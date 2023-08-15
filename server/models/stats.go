e930b1320fea2fe05cd07303035a26ef15003df3
Ashutosh Das <git.pyprism@gmail.com>
2023-08-16 00:38:13 +0600

    added new model method

--- server/models/stats.go
+++ server/models/stats.go
@@ -9,5 +9,17 @@ type Stat struct {
 	Network   Network
 	DeviceID  int
 	Device    Device
-	Latency   int `gorm:"not null"`
+	Latency   float32 `gorm:"not null"`
+}
+
+// CreateStat creates a new stat.
+func (s *Stat) CreateStat(networkID int, deviceID int, latency float32) error {
+	s.NetworkID = networkID
+	s.DeviceID = deviceID
+	s.Latency = latency
+	err := DB.Create(s)
+	if err.Error != nil {
+		return err.Error
+	}
+	return nil
 }
