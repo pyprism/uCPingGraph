eff5abbbfff5c58fa36d2ce05557254bdb8f558a
Ashutosh Das <git.pyprism@gmail.com>
2023-08-15 19:42:33 +0600

    added new prompt

--- server/cmd/deviceAdd.go
+++ server/cmd/deviceAdd.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/pyprism/uCPingGraph/models"
+	"github.com/pyprism/uCPingGraph/prompts"
 	"github.com/spf13/cobra"
 )
 
@@ -15,5 +16,6 @@ var deviceAddCmd = &cobra.Command{
 	Long:  `Add a network to the database.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		models.ConnectDb()
+		prompts.AddNewDevice()
 	},
 }
