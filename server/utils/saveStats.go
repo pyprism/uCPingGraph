87e3b72622f1ee0ca54a9d1e68e49ca9add0e825
Ashutosh Das <git.pyprism@gmail.com>
2024-02-21 02:00:10 +0600

    added new save stats func

---
+++ server/utils/saveStats.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"errors"
+	"github.com/pyprism/uCPingGraph/models"
+)
+
+func SaveStats(token string, latency float32) error {
+	device := models.Device{}
+	stat := models.Stat{}
+
+	// get device id and network id from token
+	deviceID, networkID, err := device.GetDeviceByToken(token)
+	if err != nil {
+		return errors.New("authorization header is invalid")
+	}
+
+	// create new stat
+	err = stat.CreateStat(networkID, int(deviceID), latency)
+	if err != nil {
+		return errors.New("db error: " + err.Error())
+	}
+	return nil
+}
