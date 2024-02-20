e7390bc56a9e023ba9743c718c6b66d04b4375f6
Ashutosh Das <git.pyprism@gmail.com>
2024-02-21 02:01:11 +0600

    wip on generate data

---
+++ server/cmd/generate.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"github.com/pyprism/uCPingGraph/models"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(generateData)
+}
+
+var generateData = &cobra.Command{
+	Use:   "generate",
+	Short: "Generate dummy data",
+	Long:  `Generate dummy data for development`,
+	Run: func(cmd *cobra.Command, args []string) {
+		models.ConnectDb()
+	},
+}
