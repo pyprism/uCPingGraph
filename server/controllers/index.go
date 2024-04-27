b754c5bcc46de2e26641299d222ba4421d028589
Ashutosh Das <git.pyprism@gmail.com>
2024-04-27 23:18:39 +0600

    added network db call

--- server/controllers/index.go
+++ server/controllers/index.go
@@ -2,14 +2,24 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/pyprism/uCPingGraph/models"
+	"log"
 	"net/http"
 )
 
 type IndexController struct{}
 
 func (n *IndexController) Home(c *gin.Context) {
+	networkModel := models.Network{}
+	deviceModel := models.Device{}
+	networks, err := networkModel.GetAllNetworkName()
+	devices, err := deviceModel.Get
+	if err != nil {
+		log.Println(err.Error())
+	}
 
 	c.HTML(http.StatusOK, "index.html", gin.H{
 		"htmlTitle": "μCPingGraph: Home",
+		"networks":  networks,
 	})
 }
