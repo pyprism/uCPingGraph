0e83362ef87ee45dafcfc9750324c6ddad74b5d4
Ashutosh Das <git.pyprism@gmail.com>
2024-05-03 11:40:04 +0600

    changed port

--- server/routers/config.go
+++ server/routers/config.go
@@ -7,7 +7,7 @@ import (
 
 func Init() {
 	r := NewRouter()
-	serverPort := utils.GetEnv("SERVER_PORT", "8080")
+	serverPort := utils.GetEnv("SERVER_PORT", "8880")
 	fmt.Println("Server running on http://127.0.0.1:" + serverPort)
 	err := r.Run(":" + serverPort)
 	if err != nil {
