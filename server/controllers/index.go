c73b1f9e8e855d635fbd80cd6fb0fa94bb9aa8bf
Ashutosh Das <git.pyprism@gmail.com>
2026-02-21 16:16:41 +0600

    refactored IndexController to simplify code and improve readability

--- server/controllers/index.go
+++ server/controllers/index.go
@@ -1,96 +1,15 @@
 package controllers
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/pyprism/uCPingGraph/models"
-	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type IndexController struct{}
 
-type network struct {
-	Name string `json:"name" binding:"required"`
-}
-
-type statsPost struct {
-	NetworkName string `json:"network_name" binging:"required"`
-	DeviceName  string `json:"device_name" binging:"required"`
-	Minute      int    `json:"minute" binging:"required"`
-}
-
-// Home route /
 func (n *IndexController) Home(c *gin.Context) {
-	networkModel := models.Network{}
-	networks, err := networkModel.GetAllNetworkName()
-	if err != nil {
-		log.Println(err.Error())
-	}
-
 	c.HTML(http.StatusOK, "index.html", gin.H{
-		"htmlTitle": "μCPingGraph: Home",
-		"networks":  networks,
+		"htmlTitle": "uCPingGraph Dashboard",
 	})
-}
-
-// GetDeviceList route /device/
-func (n *IndexController) GetDeviceList(c *gin.Context) {
-	var network network
-	err := c.BindJSON(&network)
-	if err != nil {
-		log.Println(err.Error())
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	}
-
-	deviceModel := models.Device{}
-	networkModel := models.Network{}
-
-	networkId, err := networkModel.GetNetworkIdByName(network.Name)
-	if err != nil {
-		log.Println(err.Error())
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	}
-
-	devices, err := deviceModel.GetDevicesByNetwork(int(networkId))
-
-	if err != nil {
-		log.Println(err.Error())
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	}
-
-	c.JSON(http.StatusOK, devices)
-
-}
-
-// Chart returns json for chart generation; route /chart/
-func (n *IndexController) Chart(c *gin.Context) {
-	var statusPost statsPost
-	err := c.BindJSON(&statusPost)
-	if err != nil {
-		log.Println(err.Error())
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	}
-
-	deviceModel := models.Device{}
-	networkModel := models.Network{}
-	statModel := models.Stat{}
-
-	networkId, err := networkModel.GetNetworkIdByName(statusPost.NetworkName)
-	if err != nil {
-		log.Println(err.Error())
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	}
-
-	deviceId, err := deviceModel.GetDeviceIdByName(statusPost.DeviceName, networkId)
-	if err != nil {
-		log.Println(err.Error())
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	}
-
-	data, err := statModel.GetStats(networkId, deviceId, statusPost.Minute)
-	if err != nil {
-		log.Println(err.Error())
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	}
-	c.JSON(http.StatusOK, data)
 }
