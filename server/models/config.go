ca371b04e48df9990cdeb407c7dc764cf666f92e
Ashutosh Das <git.pyprism@gmail.com>
2026-02-21 16:20:14 +0600

    refactored database connection logic to improve error handling and support configurable database paths

--- server/models/config.go
+++ server/models/config.go
@@ -1,21 +1,40 @@
 package models
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/pyprism/uCPingGraph/utils"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"log"
 )
 
 var DB *gorm.DB
 
-func ConnectDb() {
-	database, err := gorm.Open(sqlite.Open("./storage/db/uCPingGraph.db"), &gorm.Config{})
+func ConnectDb() error {
+	path := utils.GetEnv("DB_PATH", "./storage/db/uCPingGraph.db")
+	return ConnectDbWithPath(path)
+}
 
-	if err != nil {
-		log.Fatalln("Failed to connect to database!")
+func ConnectDbWithPath(path string) error {
+	if err := os.MkdirAll(filepath.Dir(path), 0o755); err != nil {
+		return fmt.Errorf("create db directory: %w", err)
 	}
 
-	database.AutoMigrate(&Network{}, &Device{}, &Stat{})
+	database, err := gorm.Open(sqlite.Open(path), &gorm.Config{})
+	if err != nil {
+		return fmt.Errorf("connect db: %w", err)
+	}
+
+	if err := database.AutoMigrate(&Network{}, &Device{}, &Stat{}); err != nil {
+		return fmt.Errorf("auto-migrate db: %w", err)
+	}
 
 	DB = database
+	return nil
+}
+
+func SetDB(db *gorm.DB) {
+	DB = db
 }
