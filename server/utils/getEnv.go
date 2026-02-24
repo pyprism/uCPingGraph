fc4ab8d43696da83f2b03a255544d024faf6f607
Ashutosh Das <git.pyprism@gmail.com>
2026-02-24 21:58:27 +0600

    fixed: correct typo in GetEnv function documentation

--- server/utils/getEnv.go
+++ server/utils/getEnv.go
@@ -2,7 +2,7 @@ package utils
 
 import "os"
 
-// GetEnv get key environment variable if exist otherwise return defalutValue
+// GetEnv get key environment variable if exist otherwise return defaultValue
 func GetEnv(key, defaultValue string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
