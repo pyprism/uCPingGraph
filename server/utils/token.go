fb0bbac4d3213f1c4cb58c214968ed68c45ac942
Ashutosh Das <git.pyprism@gmail.com>
2026-02-24 21:57:55 +0600

    refactored enhance GenToken to use cryptographically secure random generation

--- server/utils/token.go
+++ server/utils/token.go
@@ -1,13 +1,21 @@
 package utils
 
-import "math/rand"
+import (
+	"crypto/rand"
+	"math/big"
+)
 
-// GenToken generates a random string based on the size provided
+// GenToken generates a cryptographically secure random string based on the size provided.
 func GenToken(size int) string {
-	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-	var token = make([]rune, size)
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	token := make([]byte, size)
+	maxIdx := big.NewInt(int64(len(letters)))
 	for i := range token {
-		token[i] = letters[rand.Intn(len(letters))]
+		n, err := rand.Int(rand.Reader, maxIdx)
+		if err != nil {
+			panic("crypto/rand failed: " + err.Error())
+		}
+		token[i] = letters[n.Int64()]
 	}
 	return string(token)
 
